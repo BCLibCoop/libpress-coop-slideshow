5a3d5ed868e4e5d74ab704567e5358ccf6612ab9
Sam Edwards <sam.edwards@bc.libraries.coop>
2024-02-13 15:44:12 -0800

    v3.3.5

--- coop-slideshow.php
+++ coop-slideshow.php
@@ -18,7 +18,7 @@
  * @wordpress-plugin
  * Plugin Name:       Coop Slideshow Admin
  * Description:       Slideshow setup configurator
- * Version:           3.3.4
+ * Version:           3.3.5
  * Network:           true
  * Requires at least: 5.2
  * Requires PHP:      7.0
